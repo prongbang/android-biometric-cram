b821acfb7f7ba193d842032341951ef3acc2022c
prongbang <prongbang@gmail.com>
2023-11-02 10:12:17 +0700

    initial

--- app/src/main/java/com/prongbang/androidbiometricsignature/MainActivity.kt
+++ app/src/main/java/com/prongbang/androidbiometricsignature/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import com.prongbang.androidbiometricsignature.databinding.ActivityMainBinding
-import com.prongbang.androidbiometricsignature.server.MyServer
 import com.prongbang.biometricsignature.Biometric
 import com.prongbang.biometricsignature.SignatureBiometricPromptManager
 import com.prongbang.biometricsignature.key.KeyStoreAliasKey
@@ -16,29 +15,19 @@ class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
-    private val myServer by lazy { MyServer() }
-
-    private val userId = 1
-
     private val customKeyStoreAliasKey = object : KeyStoreAliasKey {
         override fun key(): String = "com.prongbang.signx.seckey"
     }
+    
     private val promptInfo = Biometric.PromptInfo(
         title = "BIOMETRIC",
         subtitle = "Please scan biometric to Login Application",
         description = "description here",
         negativeButton = "CANCEL"
     )
-    private var nonce: String = ""
 
     private val signBiometricSignature = object : BiometricSignature() {
-        override fun payload(): String {
-            // TODO Step: 2.1 Request
-            val challenge = myServer.challengeRequest(userId)
-            nonce = UUID.randomUUID().toString()
-
-            return challenge + nonce
-        }
+        override fun payload(): String = "hello"
     }
 
     private val registrationBiometricPromptManager by lazy {
@@ -73,9 +62,7 @@ class MainActivity : AppCompatActivity() {
     private fun initView() {
         binding.apply {
 
-            // TODO Step: 1 Registration
             registrationButton.setOnClickListener {
-
                 registrationBiometricPromptManager.createKeyPair(
                     promptInfo,
                     object : SignatureBiometricPromptManager.Result {
@@ -84,10 +71,6 @@ class MainActivity : AppCompatActivity() {
                                 Biometric.Status.SUCCEEDED -> {
                                     val publicKey = biometric.keyPair?.publicKey
                                     Log.i("SUCCEEDED", "PublicKey: $publicKey")
-
-                                    // TODO Step: 1.1 Registration
-                                    val response = myServer.registration(userId, publicKey)
-                                    Log.i("SUCCEEDED", "Registration: $response")
                                 }
                                 Biometric.Status.ERROR -> {
                                     Log.i("ERROR", "ERROR ${biometric.error}")
@@ -106,10 +89,7 @@ class MainActivity : AppCompatActivity() {
                     })
             }
 
-            // TODO Step: 2 Sign & Verify on API
             signButton.setOnClickListener {
-
-                // TODO Step: 2.1 Sign
                 signBiometricPromptManager.sign(
                     promptInfo,
                     object : SignatureBiometricPromptManager.Result {
@@ -118,23 +98,6 @@ class MainActivity : AppCompatActivity() {
                                 Biometric.Status.SUCCEEDED -> {
                                     val signature = biometric.signature
                                     Log.i("SUCCEEDED", "signature: $signature")
-
-                                    // TODO Step: 2.2 Verify
-                                    signature?.let {
-                                        val response = myServer.challengeVerify(
-                                            userId,
-                                            it.signature,
-                                            it.payload,
-                                            nonce
-                                        )
-                                        Log.i("SUCCEEDED", "verify: $response")
-
-                                        AlertDialog.Builder(this@MainActivity)
-                                            .setTitle("Signature")
-                                            .setMessage("verify: $response")
-                                            .create()
-                                            .show()
-                                    }
                                 }
                                 Biometric.Status.ERROR -> {
                                     Log.i("ERROR", "ERROR ${biometric.error}")
